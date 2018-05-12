5f40c6b9661537ab8af40fdb06f10452fe9b1b54
Tschekalinskij <tsc@mbp498.iis.fhg.de>
2018-05-12 13:53:13 +0200

    Created project

---
+++ Pods/Target Support Files/Pods-ShS_Debug/Pods-ShS_Debug-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShS_Debug : NSObject
+@end
+@implementation PodsDummy_Pods_ShS_Debug
+@end
---
+++ ShS_Debug/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ShS_Debug
+//
+//  Created by Tschekalinskij, Alexander on 12.05.18.
+//  Copyright © 2018 Tschekalinskij, Alexander. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
